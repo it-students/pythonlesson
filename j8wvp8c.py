4ff45c424d8ffa7499932dcc097152186e627dde
kinjyo Takami <n20006@std.it-college.ac.jp>
2020-07-01 00:04:31 +0900

    submit

---
+++ j8wvp8c.py
@@ -0,0 +1,10 @@
+tc = ['GOT','Narcos', 'Vice']
+i = 0
+for show in tv:
+    new = tc[i]
+    new = new.upper()
+    tc[i] = new
+    i += 1
+
+print(tc)
+
---
+++ zcvgklh.py
@@ -0,0 +1,12 @@
+tv = ['FOT', 'Narcos', 'Vice']
+coms = ['Arrested Development', 'friends', 'Always Sunny']
+all_shows = []
+
+for ahow in tv:
+    show = show.upper()
+    all_shows.append(show)
+
+for show in coms:
+    show = shows.append(show)
+
+print(all_shows)
